769fb87122e98fe1965dd2bd9ec34b87f50dafef
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-02-18 09:53:10 +0530

    background color changed

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ const App = () => {
     <TodoProvider
       value={{ todos, addTodo, updateTodo, toggleCompleted, deleteTodo }}
     >
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="bg-[#172842] min-h-screen py-8 container rounded-xl">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">My Todos</h1>
           <div className="mb-4">
